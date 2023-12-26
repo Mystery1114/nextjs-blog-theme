bba83e3e91aafb6e99316fe95f86ec6f5c881729
Mystery1114 <113797175+Mystery1114@users.noreply.github.com>
2023-12-27 04:52:57 +0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,125 @@
-import React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en" className="theme-compiled">
-        <Head />
-        <body
-          className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
-        >
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
-
-export default MyDocument;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Muhammad Abubakar - Social Media Manager</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            line-height: 1.6;
+            background-color: #f9f9f9;
+        }
+        header {
+            background-color: #333;
+            color: #fff;
+            padding: 2rem 0;
+            text-align: center;
+            margin-bottom: 2rem;
+        }
+        .container {
+            width: 80%;
+            margin: auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            border-radius: 5px;
+        }
+        h1 {
+            font-size: 2.5rem;
+            margin-bottom: 5px;
+        }
+        p {
+            font-size: 1.2rem;
+            margin: 0;
+        }
+        .main-info {
+            padding: 20px;
+            margin-bottom: 20px;
+            border-bottom: 1px solid #ccc;
+        }
+        h2 {
+            margin-bottom: 15px;
+            font-size: 1.8rem;
+            color: #333;
+        }
+        ul {
+            padding-left: 20px;
+        }
+        ul li {
+            margin-bottom: 10px;
+            font-size: 1.1rem;
+            color: #444;
+        }
+        h3 {
+            margin-bottom: 10px;
+            font-size: 1.5rem;
+            color: #333;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Muhammad Abubakar</h1>
+        <p>Social Media Manager</p>
+    </header>
+    <div class="container">
+        <div class="main-info">
+            <h2>Contact Information</h2>
+            <ul>
+                <li><strong>Email:</strong> abubakerwainsmalik@gmail.com</li>
+                <li><strong>Phone:</strong> 03089608817</li>
+                <li><strong>Address:</strong> Toheed Colony Shujanad Road larr, Multan</li>
+            </ul>
+        </div>
+        
+        <div class="main-info">
+            <h2>Skills</h2>
+            <ul>
+                <li>Sales</li>
+                <li>Team Building</li>
+                <li>Problem Solving</li>
+                <li>IT Skills</li>
+            </ul>
+        </div>
+        
+        <div class="main-info">
+            <h2>Objective</h2>
+            <p>To leverage my strong IT skills and expertise in a challenging environment that allows me to utilize my creativity and problem-solving abilities to achieve organizational goals.</p>
+        </div>
+        
+        <div class="main-info">
+            <h2>Experience</h2>
+            <h3>Social Media Manager</h3>
+            <p><strong>Laeeque Rafia Hospital & Institute of Health Sciences, Multan</strong> | Aug 2021 - Aug 2023</p>
+            <ul>
+                <li>Orchestrated effective strategies to elevate online presence and drive engagement for the hospital and the institute.</li>
+                <li>Created and managed content across various platforms, such as Facebook, Twitter, Instagram, and YouTube.</li>
+                <li>Monitored and analyzed social media metrics, such as followers, engagement, and conversions.</li>
+                <li>Coordinated with other departments, such as marketing, PR, and IT, to ensure alignment and consistency of the brand image and message.</li>
+            </ul>
+            
+            <!-- Add other experiences similarly -->
+            
+        </div>
+        
+        <div class="main-info">
+            <h2>Education</h2>
+            <h3>BS in Communication Studies</h3>
+            <p><strong>Bahauddin Zakariya University, Multan</strong> | 2018 - 2022</p>
+            <ul>
+                <li>GPA: 3.1/4.0</li>
+                <li>Completed several online courses on digital marketing, SEO, and analytics.</li>
+            </ul>
+            
+            <!-- Add other education similarly -->
+            
+        </div>
+        
+        <!-- You can continue adding other sections like Certifications, Projects, etc. -->
+        
+    </div>
+</body>
+</html>
